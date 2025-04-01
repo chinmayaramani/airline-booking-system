0c86a95e841b6b0d210589611faa66a79d0444e0
chinmayaramani <ramanichinmay@gmail.com>
2025-03-31 22:20:32 -0400

    Updated flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -41,9 +41,11 @@ def register():
 def get_db_connection():
     return pymysql.connect(
         host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PWD"),
         database=os.getenv("DB_NAME"),
+        ssl={'ca': '/etc/ssl/certs/ca-certificates.crt'},  # Required for Aiven
         cursorclass=pymysql.cursors.DictCursor
     )
 
